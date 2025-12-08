f7140203b34b39be8b919fc33056bfc5644355a6
dinesh <dineshsuwalkya31@gmail.com>
2025-12-08 23:32:33 +0530

    frontend - start creating Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,18 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import './App.css';
+
+
 function App() {
 
   return (
-    <>
-      <div className="min-h-screen bg-richblack-900">
-      {/* Using custom fonts */}
-      <h1 className="font-inter text-4xl text-caribbeangreen-100">
-        Heading with Inter Font
-      </h1>
-      
-      <h2 className="font-edu-sa text-3xl text-yellow-50">
-        Cursive Font Heading
-      </h2>
-      
-      <code className="font-mono text-pink-200">
-        const code = "with Roboto Mono";
-      </code>
+    <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+     
+          <Routes>
+              <Route path="/" element= {<Home/> } />
+          </Routes>
 
-      {/* Using custom colors */}
-      <div className="bg-richblue-400 text-white p-6">
-        Rich Blue Background
-      </div>
-
-      <button className="bg-caribbeangreen-200 text-richblack-900 px-6 py-3 rounded">
-        Caribbean Green Button
-      </button>
-
-      {/* Using custom max-width */}
-      <div className="max-w-(--width-maxContent) mx-auto">
-        Content with custom max width
-      </div>
-
-      <div className="max-w-(--width-maxContentTab) mx-auto">
-        Tablet content width
-      </div>
     </div>
-
-
-    </>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaArrowRight } from "react-icons/fa";
+
+const Home = () => {
+  return (
+    <div>
+                {/* Section 1 */}
+    <div className='relative mx-auto flex flex-col w-11/12 items-center
+    text-white justify-between'>
+
+            <Link to={"/signup"}>
+
+                <div className=' group mt-16 p-1 mx-auto rounded-full bg-richblack-800 font-bold text-richblack-200
+                transition-all duration-200 hover:scale-95 w-fit'>
+
+                    <div className='flex flex-row items-center gap-2 rounded-full px-10 py-[5px]
+                    transition-all duration-200 group-hover:bg-richblack-900 '>
+                        <p>Become an Instructor</p>
+                        <FaArrowRight />
+                    </div>
+
+                </div>
+
+            </Link>
+
+            <div>
+                  Empower Your Future with
+                  <HighlightText text={"Coding Skills"} />
+            </div>
+    </div>
+
+
+
+                {/* Section 2 */}
+                {/* Section 3 */}
+                {/* Footer */}
+
+
+    </div>
+  )
+}
+
+export default Home;
