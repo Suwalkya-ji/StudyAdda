ce6d801d0988f9b1ed67e27008c7f6be5259cf2c
dinesh <dineshsuwalkya31@gmail.com>
2025-12-15 20:19:14 +0530

    frontend - Continue Navbar

--- src/component/common/Navbar.jsx
+++ src/component/common/Navbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, matchPath } from 'react-router-dom'
 import logo from '../../assets/Logo/Logo-Full-Light.png'
-import {Navbarlinks} from '../../data/navbar-links'
+import {NavbarLinks} from '../../data/navbar-links'
+import { useLocation } from 'react-router-dom'
 
 const Navbar = () => {
+
+    const location = useLocation();
+    const matchRoute = (route) => {
+        return matchPath({path:route}, location.pathname);
+    }
+
+
   return (
     <div className='flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700'>
         
@@ -14,18 +22,33 @@ const Navbar = () => {
                 <img src={logo} width={160} height={42} loading='lazy' />            
             </Link>
 
-            {/* nav links */}
+            {/* Nav links */}
 
             <nav>
                 <ul className='flex gap-x-6 text-richblack-25'>
                     {
-                        Navbarlinks.map((link, index) => {
-                            
-                        })
-                    }
+                        NavbarLinks.map((link, index) => (
+                            <li key={index}>
+                                {
+                                    link.title === 'Catalog' ? (<div></div>) : (
+                                        <Link to={link?.path}>
+                                            <p className={`${matchRoute(link?.path) ? "text-yellow-200" : "text-richblack-25"}`}>
+                                                {link.title}
+                                            </p>
+                                        </Link>
+                                    )
+                                }
+                            </li>
+                        )  )
+                    } 
 
                 </ul>
             </nav>
+
+            {/* Login/Signup/DashBoard */}
+            
+
+
 
             </div>
 
