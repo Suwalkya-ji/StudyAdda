9ed5df9bfd519f51092f5da130abfbfb02aec352
dinesh <dineshsuwalkya31@gmail.com>
2025-12-18 19:37:27 +0530

    frontend - changes in Navbar

--- src/component/common/Navbar.jsx
+++ src/component/common/Navbar.jsx
@@ -11,16 +11,6 @@ import { categories } from '../../services/api'
 import { IoIosArrowDropdownCircle } from "react-icons/io";
 
 
-    const ssubLinks = [
-        {
-            title: "python",
-            link: '/catalog/python'
-        },
-        {
-            title: "web dev",
-            link: "/catalog/web-devlopment"
-        }
-    ]
 
 const Navbar = () => {
 
@@ -31,21 +21,22 @@ const Navbar = () => {
 
 
     const [subLinks, setSubLinks] = useState([]);
+    const [loading, setLoading] = useState(false)
 
-    const fetchSubLinks = async() => {
-                try {
-                    const result = await apiConnector("GET", categories.CATEGORIES_API) ;
-                    console.log("printing sublinks results: ", result);
-                    setSubLinks(result.data.data)              
-                }
-                catch (error) {
-                    console.log("Could not fetch the category list", error)
-                }
-            }
 
-    useEffect( () => {
-          fetchSubLinks();  
-    },[])
+   
+  useEffect(() => {
+    ;(async () => {
+      setLoading(true)
+      try {
+        const res = await apiConnector("GET", categories.CATEGORIES_API)
+        setSubLinks(res.data.data)
+      } catch (error) {
+        console.log("Could not fetch Categories.", error)
+      }
+      setLoading(false)
+    })()
+  }, [])
 
     const matchRoute = (route) => {
         return matchPath({path:route}, location.pathname);
@@ -73,25 +64,35 @@ const Navbar = () => {
                                         <div className='relative flex items-center gap-2 group'>
                                             <p>{link.title}</p>
                                     <IoIosArrowDropdownCircle />
-
-                                    <div className='invisible absolute left-[50%] translate-x-[-50%]  translate-y-[30%] top-[50%] flex flex-col rounded-md bg-richblack-5 p-4 text-richblack-900 opacity-0 transition-all duration-200 group-hover:visible group-hover:opacity-100 lg:w-[300px]'>
-
-                                        <div className='abolute left-[50%] top-0 translate-x-[650%] translate-y-[-120%] h-6 w-6 rotate-45 rounded bg-richblack-5 '>
-                                        </div>
-
-                                        {
-                                            subLinks.length ? (
-                                                
-                                                    subLinks.map((subLink, index) => (
-                                                        <Link to={`${subLink.link}`} key={index}>
-                                                            <p>{subLink.title}</p>
-                                                        </Link>
-                                                    ))
-                                                
-                                            ) : (<div> </div>) 
-                                        }
-
-                                    </div>
+                       
+                       
+                                       <div className="invisible absolute left-[50%] top-[50%] z-[1000] flex w-[200px] translate-x-[-50%] translate-y-[3em] flex-col rounded-lg bg-richblack-5 p-4 text-richblack-900 opacity-0 transition-all duration-150 group-hover:visible group-hover:translate-y-[1.65em] group-hover:opacity-100 lg:w-[300px]">
+                        <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-richblack-5"></div>
+                        {loading ? (
+                          <p className="text-center">Loading...</p>
+                        ) : subLinks?.length > 0 ?   (
+                          <>
+                            {subLinks
+                              ?.filter(
+                                (subLink) => subLink?.courses?.length > 0
+                              )
+                              ?.map((subLink, i) => (
+                                <Link
+                                  to={`/catalog/${subLink.name
+                                    .split(" ")
+                                    .join("-")
+                                    .toLowerCase()}`}
+                                  className="rounded-lg bg-transparent py-4 pl-4 hover:bg-richblack-50"
+                                  key={i}
+                                >
+                                  <p>{subLink.name}</p>
+                                </Link>
+                              ))}
+                          </>
+                        ) : (
+                          <p className="text-center">No Courses Found</p>
+                        )}
+                                         </div>
 
                                         </div>
                                     ) : (
