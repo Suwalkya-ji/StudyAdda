377b9e74aec638a37d9bdfb7bb790de20fcb0923
dinesh <dineshsuwalkya31@gmail.com>
2026-02-07 01:23:01 +0530

    cloudinary

--- Server/config/cloudinary.js
+++ Server/config/cloudinary.js
@@ -1,15 +1,15 @@
-const cloudinary = require("cloudinary").v2;   
+const cloudinary = require("cloudinary").v2;
 
 exports.cloudinaryConnect = () => {
-    try{
-        cloudinary.config({
-			//!    ########   Configuring the Cloudinary to Upload MEDIA ########
-            cloud_name: process.env.CLOUD_NAME,
-            api_key: process.env.API_KEY,
-            api_secret: process.env.API_SECRET,
-        });
-    }
-    catch(error) {
-        console.log(error);
-    }
-}
+  try {
+    cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+
+    console.log("Cloudinary connected");
+  } catch (error) {
+    console.error("Cloudinary config error:", error);
+  }
+};
